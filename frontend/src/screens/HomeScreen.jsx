8f59ad6978ad94404587ba059a6d4ed8c89aebf3
Ole Demay <olesyademy@gmail.com>
2023-08-21 15:54:10 +0200

    products fetched from backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,20 @@
-import React from "react";
 import Product from "../components/Product";
-import products from "../products";
 import { Row, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <Row>
       {products.map((product) => (
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,42 @@
 import { useParams } from "react-router-dom";
-import products from "../products";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
-  console.log(product);
 
-  return <div>ProductScreen</div>;
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
+
+  return (
+    <div>
+      <h2>{product.name}</h2>
+      <img src={product.image} alt={product.name} />
+
+      <div>Description: {product.description}</div>
+      <div>Price: ${product.price}</div>
+      <div>
+        {product.rating}
+        {`${product.numReviews} reviews`}
+      </div>
+      <div> {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}</div>
+
+      <button
+        className="btn-block"
+        type="button"
+        disabled={product.countInStock === 0}
+      >
+        Add To Cart
+      </button>
+    </div>
+  );
 };
 
 export default ProductScreen;
